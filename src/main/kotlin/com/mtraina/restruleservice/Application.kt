11cd97339dbb77e4ce7323992f037481b295727a
mtraina <matteo.traina.mail@gmail.com>
2019-07-02 23:21:17 +0200

    webflux added part II

--- src/main/kotlin/com/mtraina/restruleservice/Application.kt
+++ src/main/kotlin/com/mtraina/restruleservice/Application.kt
@@ -1,13 +1,16 @@
 package com.mtraina.restruleservice
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.support.GenericApplicationContext
 import org.springframework.context.support.beans
-import org.springframework.web.reactive.function.server.RouterFunctions
-import org.springframework.web.server.adapter.WebHttpHandlerBuilder
 
 @SpringBootApplication
-open class Application
+open class Application {
+    @Autowired
+    fun register(ctx: GenericApplicationContext) = beans().initialize(ctx)
+}
 
 fun main(args: Array<String>) {
     SpringApplication.run(Application::class.java, *args)
@@ -16,8 +19,8 @@ fun main(args: Array<String>) {
 fun beans() = beans {
     bean<RuleHandler>()
     bean<RuleService>()
-    bean<Routes>()
-    bean(WebHttpHandlerBuilder.WEB_HANDLER_BEAN_NAME) {
-        RouterFunctions.toWebHandler(ref<Routes>().router())
-    }
+    //bean<Routes>()
+    //bean(WebHttpHandlerBuilder.WEB_HANDLER_BEAN_NAME) {
+    //    RouterFunctions.toWebHandler(ref<Routes>().router())
+    //}
 }
--- src/main/kotlin/com/mtraina/restruleservice/Routes.kt
+++ src/main/kotlin/com/mtraina/restruleservice/Routes.kt
@@ -1,11 +1,15 @@
 package com.mtraina.restruleservice
 
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.http.MediaType.APPLICATION_JSON
 import org.springframework.web.reactive.function.server.router
 
-class Routes(private val ruleHandler: RuleHandler) {
+@Configuration
+open class Routes(private val ruleHandler: RuleHandler) {
 
-    fun router() = router {
+    @Bean
+    open fun router() = router {
         "/api".nest {
             accept(APPLICATION_JSON).nest {
                 GET("/rules", ruleHandler::findAll)
